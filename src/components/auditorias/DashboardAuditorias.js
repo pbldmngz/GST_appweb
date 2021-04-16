73590d395c1e8daf2eec65c5f1d6e8bb4d275d75
Spectra <32307513+pbldmngz@users.noreply.github.com>
2021-04-15 19:38:22 -0700

    No mostrar auditorías ya hechas
    Se necesita una forma de saber si un usuario ya realizó cierta auditoría, lo más fácil es consultar en la colección de respuestas por algo que tenga la id de la auditoria y el del usuario, con que haya uno basta, no se necesitan ver las respuestas que publicó.

--- src/components/auditorias/DashboardAuditorias.js
+++ src/components/auditorias/DashboardAuditorias.js
@@ -8,15 +8,45 @@ import { Redirect } from 'react-router'
 
 class DashboardAuditorias extends Component {
     render() {
-        const { auditorias, auth, userLevel } = this.props
+        const { auditorias, respuestas, auth, userLevel } = this.props
 
         if (!auth.uid) return <Redirect to="/signin"/>
-        if (userLevel != 0) return <Redirect to="/" />
+        // if (userLevel != 0) return <Redirect to="/" />
+        console.log("userLevel: ", userLevel)
+        console.log("respuestas", respuestas)
 
-        if (auditorias) {
+
+        // ---> Con esto los usuarios no ven las que ya respondieron <---
+        // Se podría mejorar mandando un prop de que ya está resuelta
+        // hacia la tarjeta, pero se me hace mucha bronca porque luego se tendrían
+        // Que mandar al fondo. En todo caso puedo hacer una vista de auditorias YA HECHAS
+        var filteredAuditorias = auditorias;
+
+        if (userLevel != 0 && respuestas) {
+            // console.log("0", respuestas, auth.uid)
+            const filtRespuestas = respuestas.filter(res => auth.uid == res.answeredById)
+
+            // console.log("1", filtRespuestas)
+
+            var filtID = {}
+            for (let fA in filtRespuestas){
+                console.log("fA", filtRespuestas[fA])
+                filtID[filtRespuestas[fA].auditoria] = true
+            }
+            
+            // console.log("2", filtID)
+
+            const alreadyDone = Object.keys(filtID)
+
+            // console.log("3", alreadyDone)
+
+            filteredAuditorias = auditorias.filter(aud => !alreadyDone.includes(aud.id))
+        }
+
+        if (filteredAuditorias) {
             return (
                 <div className="dashboard container">
-                    <Auditorias auditorias={auditorias} userLevel={userLevel} />
+                    <Auditorias auditorias={filteredAuditorias} userLevel={userLevel} />
                 </div>
             )
         } else {
@@ -33,6 +63,7 @@ const mapStateToProps = (state) => {
     //console.log(state)
     return {
         auditorias: state.firestore.ordered.auditorias,
+        respuestas: state.firestore.ordered.respuestas,
         auth: state.firebase.auth,
         userLevel: state.firebase.profile.userLevel
     }
@@ -40,5 +71,5 @@ const mapStateToProps = (state) => {
 
 export default compose(
     connect(mapStateToProps),
-    firestoreConnect([{ collection: "auditorias", orderBy: ["fecha_fin", "asc"]}])
+    firestoreConnect([{ collection: "auditorias", orderBy: ["fecha_fin", "asc"]}, {collection:"respuestas"}])
 )(DashboardAuditorias)
